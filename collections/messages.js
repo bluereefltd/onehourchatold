d1c13e451bb834a5052e92f889498c9d3a59c0a9
Julien Choulet <jchoulet@blue-reef-ltd.com>
2015-09-22 18:38:58 +0800

    remove insecure package add allow rules on messages collection

--- collections/messages.js
+++ collections/messages.js
@@ -1,1 +1,13 @@
 Messages = new Mongo.Collection('messages');
+
+Messages.allow({
+    insert: function (userId, msg) {
+        return (Meteor.users.findOne(userId).username === msg.author)
+    },
+    update: function () {
+        return false;
+    },
+    remove: function () {
+        return false;
+    }
+});
